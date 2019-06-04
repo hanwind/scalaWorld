cf68f432e63ed5ed7ffbe2257e68d3a7f7618092
HWX <752799184@qq.com>
2019-06-04 21:27:34 +0800

    [update]修改akka的版本为2.11来支持scala2.11，开始akka编程

--- src/main/scala/TestTrait.scala
+++ src/main/scala/TestTrait.scala
@@ -5,4 +5,13 @@ class Wrapper(val underlying: Int) extends AnyVal with Printable
 object TestTrait extends App{
   val w = new Wrapper(3)
   w.print()
+  w.getClass()
+  //定义一个函数
+  val f = (x: Int) => x + 3
+  println(f(1))
+  //闭包
+  val factor = 3
+  val multiplier = (i: Int) => i * factor
+  println(multiplier(1))
+  println(multiplier(2))
 }
--- src/main/scala/akka/testAkka.scala
+++ src/main/scala/akka/testAkka.scala
@@ -1,5 +1,16 @@
 package akka
 
-object testAkka {
+import akka.actor.{Actor, ActorSystem, Props}
+
+class EchoServer extends Actor{
+  override def receive: Receive = {
+    case msg: String => println("Echo " + msg)
+  }
+}
+object testAkka extends App{
+  val system = ActorSystem("helloAkka")
+  val echoServer = system.actorOf(Props[EchoServer])
+  echoServer ! "Hello!"
+
 
 }
